90024d678d623d55587a8a57b5932f9d88257dd0
Miles Eastwood <milotheengineer@gmail.com>
2020-02-10 22:59:44 +0000

    Atempting a different approch to the conditional statements

--- exercises.py
+++ exercises.py
@@ -19,30 +19,41 @@
 # Question 2
 # Building upon the previous exercise, write a program that takes a string as input from the user and prints the first number encountered along with the starting position of the number.
 
-# First we set the variables for the user input and initialise a counter. We need to include a second counter for the second while loop
-user_string = input("Please enter your string here: ")
-counter_str = 0
-counter_num = 0
-
-# Next we construct a while loop to check each index for a number
-while counter_str < len(user_string) and not user_string[counter_str].isnumeric():
-    counter_str += 1
-
-    # The conditional statements are used to print out the output, of whether the string contains a number or not
-    if counter_str >= len(user_string):
-        print("Sorry your string does not contain any numbers")
-
-    elif counter_str < len(user_string) and user_string[counter_str].isnumeric():
-        # This second while loop iterates through the string starting from the first instance of the number to the last instance
-        while counter_num < len(user_string) and user_string[counter_str].isnumeric():
-            counter_num += 1
-
-        # This print statement slices the string from the start of the number, to the end of the number
-        print(f"Here is your number {user_string[counter_str:counter_num]} and its index is {counter_str}")
+# # First we set the variables for the user input and initialise a counter. We need to include a second counter for the second while loop
+# user_string = input("Please enter your string here: ")
+# counter_str = 0
+# counter_num = 0
+#
+# # Next we construct a while loop to check each index for a number
+# while counter_str < len(user_string) and not user_string[counter_str].isnumeric():
+#     counter_str += 1
+#
+#     # The conditional statements are used to print out the output, of whether the string contains a number or not
+#     if counter_str >= len(user_string):
+#         print("Sorry your string does not contain any numbers")
+#
+#     elif counter_str < len(user_string) and user_string[counter_str].isnumeric():
+#         # This second while loop iterates through the string starting from the first instance of the number to the last instance
+#         while counter_num < len(user_string) and user_string[counter_str].isnumeric():
+#             counter_num += 1
+#
+#         # This print statement slices the string from the start of the number, to the end of the number
+#         print(f"Here is your number {user_string[counter_str:counter_num]} and its index starts at {counter_str}")
 
 
 # Question 3
-# 
+# Write a program that takes a string as input from the user. The string will consist of digits and your program should print out the first repdigit. A repdigit is a number where all digits in the number are the same. Examples of repdigits are 22, 77, 2222, 99999, 444444. You can also assume that each number will have differing digits unless it is a repdigit.
+
+# First we define the user input variables and initialise a counter
+user_input = (input("Please enter your number: "))
+counter = 0
+
+# Next we construct a while loop to iterate through the number
+while counter <= len(user_input) and user_input[counter + 1] == user_input[counter]:
+    counter += 1
+    var = counter
+
+    print(f"Here is your number {user_input[counter]} ")
 
 
 

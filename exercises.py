d0a129d0b86a19b519122e32cea68485dfc71624
Miles Eastwood <milotheengineer@gmail.com>
2020-02-10 11:41:50 +0000

    Finished question 2

--- exercises.py
+++ exercises.py
@@ -19,22 +19,26 @@
 # Question 2
 # Building upon the previous exercise, write a program that takes a string as input from the user and prints the first number encountered along with the starting position of the number.
 
-# First we set the variables for the user input and initialise a counter
+# First we set the variables for the user input and initialise a counter. We need to include a second counter for the second while loop
 user_string = input("Please enter your string here: ")
-counter = 0
+counter_str = 0
+counter_num = 0
 
 # Next we construct a while loop to check each index for a number
-while counter < len(user_string) and not user_string[counter].isnumeric():
-    counter += 1
+while counter_str < len(user_string) and not user_string[counter_str].isnumeric():
+    counter_str += 1
 
     # The conditional statements are used to print out the output, of whether the string contains a number or not
-    if counter >= len(user_string):
+    if counter_str >= len(user_string):
         print("Sorry your string does not contain any numbers")
 
-    elif counter < len(user_string) and user_string[counter].isnumeric():
-        while counter
+    elif counter_str < len(user_string) and user_string[counter_str].isnumeric():
+        # This second while loop iterates through the string starting from the first instance of the number to the last instance
+        while counter_num < len(user_string) and user_string[counter_str].isnumeric():
+            counter_num += 1
 
-        print(f"Here is your number {user_string[counter]} and its index is {counter}")
+        # This print statement slices the string from the start of the number, to the end of the number
+        print(f"Here is your number {user_string[counter_str:counter_num]} and its index is {counter_str}")
 
 
 
